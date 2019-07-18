ad54c1f843d09fe2d1c1a2ce5618308a560898cc
FireUnicornser <FireUnicornser@outlook.com>
2019-07-18 22:00:36 +0800

    基础版

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GitHub-Code-Get-Token
 使用GitHub Code 换取 Github Oauth2 Token
+
+>启动服务 yarn start
+
+>停止服务 yarn stop
+
+>重启服务 yarn restart
---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/nodejs
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+var axios = require('axios')
+var fs = require('fs')
+var conf = {}
+fs.readFile('./conf.json', 'utf-8', function (err, data) {
+  if (err) {
+    process.exit(0)
+  } else {
+    conf = JSON.parse(data)
+    app.use(bodyparser.urlencoded({ extended: true }));
+    app.use(bodyparser.json());
+    app.all('*', function (req, res, next) {
+      res.header('Access-Control-Allow-Origin', conf.origin);
+      res.header('Access-Control-Allow-Methods', 'POST');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+      res.header('Content-Type', 'application/json;charset=utf-8');
+      next();
+    })
+    app.post('/', function (req, res) {
+      var ret = {
+        'state': false,
+      }
+      if (typeof (req.body.code) == "undefined") {
+        res['desc'] = 'bad_parameter';
+        res.send(ret)
+      } else {
+        axios.post('https://github.com/login/oauth/access_token', {
+          'client_id': conf.client_id,
+          'client_secret': conf.client_secret,
+          'code': req.body.code
+        }, {
+            headers: {
+              'Accept': 'application/json',
+            }
+          }).then(function (response) {
+            if (typeof (response.data.error) == "undefined") {
+              ret['state'] = true
+              ret['token'] = response.data.access_token
+            } else {
+              ret['desc'] = response.data.error
+            }
+            res.send(ret)
+          }).catch(function (error) {
+            res.send({
+              'error': error
+            });
+          });
+      }
+    })
+    var server = app.listen(conf.port, function () {
+      var host = server.address().address
+      var port = server.address().port;
+    })
+  }
+})
